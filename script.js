67a84d377a0437948edce287703e862a005f9144
Johnathan Chei <j.chei175@gmail.com>
2015-07-16 00:48:42 -0400

    added switch to account for picture cards. Also might be onto something for no-repeat-card

--- script.js
+++ script.js
@@ -14,11 +14,13 @@ var deck = {
 	3:{"1":11,"2":2,"3":3,"4":4,"5":5,"6":6,"7":7,"8":8,"9":9,"10":10,"11":10,"12":10,"13":10},
 	4:{"1":11,"2":2,"3":3,"4":4,"5":5,"6":6,"7":7,"8":8,"9":9,"10":10,"11":10,"12":10,"13":10}
 };
-var dealt=[]
-var getSuit = function(number){
+var dealt=[["Joker",0]];
+var getCard = function(number){
 	var drawCard = [];
 	var cardSuit = "";
 	var cardValue = deck[number][card()];
+	var cardFace = card();
+
  switch (number){
  	case 1:
 		cardSuit = "Spades";
@@ -36,15 +38,38 @@ var getSuit = function(number){
 		cardSuit = "Diamonds";
  		console.log("Diamonds")
  		break;
- 	}
-//need way to not allow dealt cards
-drawCard.push(cardSuit);
-drawCard.push(cardValue);
+ 	};
+ 		switch (cardFace){
+		case 11:
+			cardFace = "J"
+			break;
+		case 12:
+			cardFace = "Q"
+			break;
+		case 13:
+			cardFace = "K"
+			break;
+		case 1:
+			cardFace = "A"
+			break;
+		default:
+			break;
+	};
+ //Only need to push one if all interation = true
+ //Solution? create function, if return true, push?	
+ // for (var i=0; i<dealt.length; i++){
+ // 	if (cardSuit!==dealt[i][0]&&cardValue!==dealt[i][1]){
+ // 		drawCard.push(cardSuit);
+	// 	drawCard.push(cardValue);
+ // 	} else {
+ // 		console.log("Dealt alrdy")
+ // 	}
+ // }
 return drawCard
  };
 
-var firstCard = getSuit(suit())
-var secondCard = getSuit(suit())
+var firstCard = getCard(suit())
+var secondCard = getCard(suit())
 dealt.push(firstCard)
 dealt.push(secondCard)
  
